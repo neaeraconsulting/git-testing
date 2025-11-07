ce1902a25d107d30b2b14cf9c7a21785854e07cf
jacob6838 <51723791+jacob6838@users.noreply.github.com>
2025-11-07 14:56:51 -0700

    Feature 1
    * Updat Todo.java adding whitespace
    
    * Update Todo.java adding more whitespace
    
    * feature-1 final

--- backend/src/main/java/com/example/todoapp/model/Todo.java
+++ backend/src/main/java/com/example/todoapp/model/Todo.java
@@ -10,6 +10,8 @@ public class Todo {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
+
+
     @Column(nullable = false)
     private String title;
     
@@ -24,24 +26,36 @@ public class Todo {
         this.completed = completed;
     }
     
+    
     public Long getId() {
         return id;
     }
+
+
     public void setId(Long id) {
         this.id = id;
     }
+
+
     public String getTitle() {
         return title;
     }
+
+
     public void setTitle(String title) {
         this.title = title;
     }
     
+
     public boolean isCompleted() {
         return completed;
     }
     
+
     public void setCompleted(boolean completed) {
         this.completed = completed;
     }
+
+
+    
 }
